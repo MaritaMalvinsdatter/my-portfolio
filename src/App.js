3f1a813aa5cdd19e63c2632c2b4755497c282504
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-10-05 19:23:19 +0200

    imported/routed projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../src/components/Layout.jsx';
 import Home from '../src/pages/Home.jsx';
+import Projects from '../src/pages/Projects.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
         </Route>
       </Routes>
     </React.StrictMode>
