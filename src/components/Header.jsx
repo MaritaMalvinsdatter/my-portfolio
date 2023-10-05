ff09aad8e6cb67872f6bc769a829b1d847f43c42
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-10-05 19:24:45 +0200

    correct nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,21 @@ import { Navbar } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import styles from "../styles/Header.module.css"
 import { ReactComponent as Logo } from '../images/color_logo.svg';
+import { Link } from 'react-router-dom';
 
 
 const Header = () => {
     return (
         <Navbar expand="lg" variant="dark" className={styles.customBg}>
-            <Navbar.Brand href="#">
+            <Navbar.Brand as={Link} to="/">
                 <Logo width="400" height="auto" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
+                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
