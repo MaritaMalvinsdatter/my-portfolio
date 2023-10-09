ce1492fd56026fd54610786df6dd1ee6c8acafd6
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-10-09 11:24:53 +0200

    landingpage done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from "../styles/Home.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from '../images/color_logo.svg';
 
 function Home() {
   return (
-    <div>
-      Home Page
+    <div className={styles.container}>
+      <Logo className={styles.logo} />
+      <nav className={styles.navLinks}>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+      </nav>
     </div>
   );
 }
