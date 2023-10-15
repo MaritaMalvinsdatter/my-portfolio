79cf2ae25ac5f90334c8b54db80382236b9d2256
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-10-15 14:34:28 +0200

    added routing to about

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '../src/components/Layout.jsx';
 import Home from '../src/pages/Home.jsx';
 import Projects from '../src/pages/Projects.jsx';
+import About from '../src/pages/About.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
         </Route>
       </Routes>
     </React.StrictMode>
