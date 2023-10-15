7baed79ba6fbe4ea599a1dabdcfd59ccfc647c5b
MaritaMalvinsdatter <79151543+MaritaMalvinsdatter@users.noreply.github.com>
2023-10-15 14:35:08 +0200

    removed unused link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,6 @@ const Header = () => {
                 <Nav className="ml-auto">
                     <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
                     <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
